8e78c116bb2c5f0db14e2e5f06b00ef550c86ad4
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-11 17:40:36 +0300

    Commit 51 - Change SnackBar Styling

--- lib/helpers/show_snack_bar.dart
+++ lib/helpers/show_snack_bar.dart
@@ -5,12 +5,35 @@
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        elevation: 5,
-        showCloseIcon: true,
-        closeIconColor: Colors.white,
-        content: Text(text),
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      elevation: 8,
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
       ),
-    );
-  }
+      backgroundColor:
+          Color(0xFF71060A),
+      duration: Duration(seconds: 4),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.info, color: Colors.white),
+          SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.close, color: Colors.white),
+            onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            },
+          ),
+        ],
+      ),
+    ),
+  );
+}
